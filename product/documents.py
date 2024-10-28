9a8938c17991e62b7918ce8184951c53cafe3d80
coderatwork-7 <coder.atwork00@gmail.com>
2024-10-28 11:09:24 +0500

    Working on Suggestions

--- product/documents.py
+++ product/documents.py
@@ -1,15 +1,22 @@
 # documents.py
-from django_elasticsearch_dsl import Document
 from django_elasticsearch_dsl.registries import registry
+from django_elasticsearch_dsl import Document, fields
+
 from .models import Product
 
 @registry.register_document
 class ProductDocuments(Document):
+    title_suggest = fields.CompletionField()
+
     class Index:
-        name = 'product_index'  
+        name = 'product_index'
 
     class Django:
-        model = Product  
-        fields = ['id', 'title', 'description']
+        model = Product
+        fields = [
+            'title',
+            'description',
+        ]
 
-
+    def prepare_title_suggest(self, instance):
+        return instance.title
