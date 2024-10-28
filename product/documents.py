750caf09c30e74fbbb7b1a661e00578e5d714bfc
coderatwork-7 <coder.atwork00@gmail.com>
2024-10-28 11:41:19 +0500

    apply ngram and tokenizer

--- product/documents.py
+++ product/documents.py
@@ -1,22 +1,37 @@
 # documents.py
 from django_elasticsearch_dsl.registries import registry
 from django_elasticsearch_dsl import Document, fields
+from elasticsearch_dsl import analyzer, tokenizer, Text
+
 
 from .models import Product
 
+ngram_tokenizer = tokenizer(
+    'ngram_tokenizer',
+    'ngram',
+    min_gram=1,
+    max_gram=2,
+)
+
+# Define your n-gram analyzer
+ngram_analyzer = analyzer(
+    'ngram_analyzer',
+    tokenizer=ngram_tokenizer,
+    filter=['lowercase']
+)
 @registry.register_document
 class ProductDocuments(Document):
-    title_suggest = fields.CompletionField()
+    title = fields.TextField(analyzer='standard')
+    description = fields.TextField(analyzer='standard')
+    title_suggest = fields.CompletionField(analyzer=ngram_analyzer)
 
     class Index:
         name = 'product_index'
-
     class Django:
         model = Product
-        fields = [
-            'title',
-            'description',
-        ]
+        # fields = [
+        #     'description',
+        # ]
 
     def prepare_title_suggest(self, instance):
-        return instance.title
+        return instance.description
